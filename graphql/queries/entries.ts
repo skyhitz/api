cd86964e81f63824905e69e3ce240edaadd535a7
alejo <alejo@splice.com>
2019-02-04 16:24:17 -0500

    fix profile entries

--- graphql/queries/entries.ts
+++ graphql/queries/entries.ts
@@ -15,10 +15,16 @@ const Entries = {
   },
   async resolve(root: any, args: any, ctx: any) {
     let user = await getAuthenticatedUser(ctx);
+    if (!args.userId) {
+      return Database.models.entry.findAll({ where: args });
+    }
     if (args.userId === user.id) {
       return user.getEntryOwner({ limit: 500, offset: 0 });
     }
-    return Database.models.entry.findAll({ where: args });
+    let profileUser = await Database.models.user.findOne({
+      where: { id: args.userId }
+    });
+    return profileUser.getEntryOwner({ limit: 500, offset: 0 });
   }
 };
 
